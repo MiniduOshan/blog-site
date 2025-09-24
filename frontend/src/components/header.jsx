169bc1a5fe520f687821a0192f736da067d6ad71
MiniduOshan <126508461+MiniduOshan@users.noreply.github.com>
2025-09-24 09:55:39 +0530

    first commit

---
+++ frontend/src/components/header.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+function Header() {
+  return (
+    <div className='bg-white-600 p-1 flex space-x-6 items-center justify-between'>
+    
+        <h1 className=''>
+          Welcome to My Blog
+        </h1>
+        
+
+
+    </div>
+  );
+}
+
+export default Header;
